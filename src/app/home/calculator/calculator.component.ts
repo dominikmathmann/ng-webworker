df2222b5ef9539241682a9ef1dd5bca58d931f28
Dominik Mathmann <dominik.mathmann@gedoplan.de>
2019-07-24 09:23:49 +0200

    WebWorker

--- src/app/home/calculator/calculator.component.ts
+++ src/app/home/calculator/calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
@@ -6,9 +6,12 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss']
 })
-export class CalculatorComponent implements OnInit {
+export class CalculatorComponent {
 
   form: FormGroup;
+
+  @Input()
+  byWebWorker = false;
 
   constructor(fb: FormBuilder) {
     this.form = fb.group({
@@ -19,15 +22,24 @@ export class CalculatorComponent implements OnInit {
     });
   }
 
-  ngOnInit() {
-  }
-
 
   calculate() {
     const formValues = this.form.value;
     this.form.get('result').setValue(formValues.initValue);
     for (let i = 0; i < this.form.get('times').value; i++) {
       this.form.get('result').setValue(this.form.get('result').value * formValues.multiplier);
+    }
+  }
+
+  calculateWW() {
+    const worker = new Worker('./multiplier.worker.ts', { type: 'module' });
+    worker.onmessage = ({ data }) => {
+      this.form.get('result').setValue(data);
+    };
+
+    this.form.get('result').setValue(this.form.get('initValue').value);
+    for (let i = 0; i < this.form.get('times').value; i++) {
+      worker.postMessage(this.form.value);
     }
   }
 }
